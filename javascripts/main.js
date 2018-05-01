62e0351a0a1a553be5763fd9a4e75713655446af
Adam Wieckert <wiec1369@gmail.com>
2018-04-30 20:58:43 -0500

    Added fish to the basket

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+const moveToCart = (e) => {
+  let fish = $(e.target).closest('.fish');
+  console.log(fish);
+  $('#snagged').append(fish);
+};
+
+$('button.add').click(moveToCart);
