b8c9846b882ad6687c65060e62c07f26d31f28f3
Adam Wieckert <wiec1369@gmail.com>
2018-05-01 20:51:00 -0500

    Added the discount

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,12 +1,14 @@
 const loadFishes = require('./ajax.js');
 const writeFishes = require('./fishDom.js');
 const initializer = require('./events.js');
+const discount = require('./discount.js');
 
 const whenFishesLoad = (data) => {
   console.log('JSON Data: ', data);
   const fishArray = data.fishes;
   $('#available').append(writeFishes(fishArray));
   initializer();
+  discount();
 };
 
 const whenFishesFails = (error) => {
